b1ecd3b2ff2fbaf70e021ab492356e21bb3631d4
David Goodman <davegoodman20@gmail.com>
2020-01-20 17:13:09 -0500

    update readme and add comments

--- src/ProfileHome.js
+++ src/ProfileHome.js
@@ -7,6 +7,9 @@ export default class ProfileHome extends React.Component {
 		this.state = { shows: [] };
 	}
 
+	/* Note: I wasn't sure how to proceed with the userId prop
+	 so I just left this hardcoded the data with JSON-server. 
+	 Apologies if I overlooked anything! */
 	getFavoriteShowsByUserId() {
 		fetch(`http://localhost:3004/shows`)
 			.then(data => data.json())
